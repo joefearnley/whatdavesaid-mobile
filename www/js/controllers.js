9bbb324c7b0cb983c92a028627fc551c0076f2be
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-02 18:28:42 -0400

    Adding files and creating service to fetch them

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,4 +2,11 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, Clips) {
     $scope.clips = Clips.all();
+
+    $scope.play = function(clip) {
+        $scope.media = new Audio();
+        $scope.media.src = '/clips/' + clip.file;
+        $scope.media.load();
+        $scope.media.play();
+    };
 });
