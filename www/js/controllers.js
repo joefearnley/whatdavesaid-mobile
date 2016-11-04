79b9a49afe9759cfc734c89f3ab5d770cee312ff
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-04 18:30:53 -0400

    moving clips to a json file

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,17 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, Clips) {
-    $scope.clips = Clips.all();
+    $scope.clips = [];
+
+    Clips.all().then(function(response) {
+        $scope.clips = response.data.clips;
+    });
 
     $scope.play = function(clip) {
+        if ($scope.media) {
+            $scope.media.pause();
+        }
+
         $scope.media = new Audio();
         $scope.media.src = '/clips/' + clip.file;
         $scope.media.load();
