b1e3c9b8d062952d912282c3447dcfa0f81573ea
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-11 21:10:58 -0500

    Adding a wee bit of flash?

--- www/js/app.js
+++ www/js/app.js
@@ -14,10 +14,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
+
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
