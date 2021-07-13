9c926f4be53c2f4d10a2f9e2eaebce9fd56d2af6
Joe Fearnley <joe.fearnley@gmail.com>
2021-07-12 22:23:08 -0400

    Adding padding to list

--- ionic-react/src/components/ClipListItem.tsx
+++ ionic-react/src/components/ClipListItem.tsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 import {
   IonItem,
   IonLabel,
-  IonIcon
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol
 } from '@ionic/react';
 import { play } from 'ionicons/icons';
 import { Clip } from '../data/clips';
@@ -18,10 +21,12 @@ const ClipListItem: React.FC<ClipProps> = ({ clip }) => {
   const [ clipIsPlaying, setClipIsPlaying ] = useState(false);
 
   const playClip = () => {
+    console.log('playing clip....');
+
     const soundClip = new Howl({
       src: [clip.path],
       onend: function() {
-        console.log('Finished!');
+        console.log('finished....');
         setClipIsPlaying(clipIsPlaying);
       }
     });
@@ -33,21 +38,26 @@ const ClipListItem: React.FC<ClipProps> = ({ clip }) => {
   return (
     <IonItem button onClick={playClip} detail={false}>
       <IonLabel className="ion-padding-start">
-        <h2>
-          {
-            clipIsPlaying ?
-            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26">
-              <rect className="eq-bar eq-bar--1" x="4" y="4" width="3.7" height="8"/>
-              <rect className="eq-bar eq-bar--2" x="10.2" y="4" width="3.7" height="16"/>
-              <rect className="eq-bar eq-bar--3" x="16.3" y="4" width="3.7" height="11"/>
-              <rect className="eq-bar eq-bar--4" x="22.4" y="4" width="3.7" height="10"/>
-            </svg>
-            :
-            <IonIcon icon={play}></IonIcon>
-          }
-
-          <span className="ion-padding-start">{clip.title}</span>
-        </h2>
+        <IonGrid>
+          <IonRow className="ion-align-items-center">
+            <IonCol size="2">
+              {
+                clipIsPlaying ? 
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26">
+                  <rect className="eq-bar eq-bar--1" x="4" y="4" width="3.7" height="8"/>
+                  <rect className="eq-bar eq-bar--2" x="10.2" y="4" width="3.7" height="16"/>
+                  <rect className="eq-bar eq-bar--3" x="16.3" y="4" width="3.7" height="11"/>
+                  <rect className="eq-bar eq-bar--4" x="22.4" y="4" width="3.7" height="10"/>
+                </svg>
+                :
+                <IonIcon size="large" icon={play}></IonIcon>
+              }
+            </IonCol>
+            <IonCol size="10">
+              {clip.title}
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonLabel>
     </IonItem>
   );
